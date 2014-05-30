20b4a8554bb48607452094542d0458bbf2d9e5ae
DHOMD <DHO@dho.im>
2014-05-30 19:44:21 -0400

    Use our own deconfig
    Change-Id: I26a3bc4349bfc4528a0b11d0c435b1bdb8fa0344

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,7 @@ TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 
 # Build kernel from source
 TARGET_KERNEL_SOURCE := kernel/asus/tf700t
-TARGET_KERNEL_CONFIG := GRIMLOCK_tf700t_defconfig
+TARGET_KERNEL_CONFIG := commotio_tf700t_defconfig
 
 # Custom Tools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/asus/tf700t/releasetools/tf700t_ota_from_target_files
